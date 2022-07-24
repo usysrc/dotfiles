8f74ea213463183020aee87830d306cdc11d2ffe
usysrc <usysrc@usysrc.de>
2022-07-24 21:13:54 +0200

    added catppuccin theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,6 +17,7 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'nvim-lua/plenary.nvim'
 Plug('nvim-telescope/telescope.nvim', { tag = '0.1.0' })
+Plug('catppuccin/nvim', { as = "catppuccin" })
 
 vim.call('plug#end')
 
