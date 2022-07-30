d844120aa7bc133857d24452e736ceb782a04783
usysrc <usysrc@usysrc.de>
2022-07-30 23:43:24 +0200

    added vim-go and shortcuts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,8 +18,12 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug 'nvim-lua/plenary.nvim'
 Plug('nvim-telescope/telescope.nvim', { tag = '0.1.0' })
 Plug('catppuccin/nvim', { as = "catppuccin" })
+Plug('fatih/vim-go')
 
 vim.call('plug#end')
 
 
 vim.keymap.set('n', '<c-p>',"<Cmd>Telescope find_files<CR>")
+vim.keymap.set({'n', 'i'}, '<c-s>',"<Cmd>w<CR>")
+vim.keymap.set({'n', 'i'}, '<c-q>',"<Cmd>q<CR>")
+
